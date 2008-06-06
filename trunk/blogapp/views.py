cd47dbcbc657b80f0e15b09cb890c8a265c1126e
jarusauskas <jarusauskas@cb7e2e46-004f-0410-aa2f-81f071f90814>
2008-06-06 20:28:52 +0000

    render_to_response used instead of get_template + Context + HttpResponse

--- trunk/blogapp/views.py
+++ trunk/blogapp/views.py
@@ -1,29 +1,24 @@
 from django.template.loader import get_template
 from django.template import Context
-from django.http import HttpResponse
+from django.shortcuts import render_to_response
 
 from blogapp.models import *
 from blogapp.utilities import not_found
 
 def homepage(request):
     posts = Post.objects.all()[:5]
-    template = get_template('blog.html')
-    html = template.render(Context({'posts': posts,}))
-    return HttpResponse(html)
+    return render_to_response('blog.html', {'posts': posts,})
 
 def post_by_name(request, post_name, fragment_identifier):
     try:
         post = Post.objects.get(name=post_name)
         comments = post.comment_set.all()
-        template = get_template('blog.html')
-
-        context = Context({
+        context = {
             'posts': [post],
             'comments': comments,
             'title': post.title + " // ",
-            })
-        html = template.render(context)
-        return HttpResponse(html)
+        }
+        return render_to_response('blog.html', context)
     except:
         return not_found(request, message="Sorry, the requested post does not exist.")
 
@@ -31,16 +26,13 @@ def posts_by_tag(request, tag_name):
     try:
         tag = Tag.objects.get(name=tag_name)
         posts = tag.post_set.all()[:5]
-        template = get_template('blog.html')
-        html = template.render(Context({'posts': posts,}))
-        return HttpResponse(html)
+        return render_to_response('blog.html', {'posts': posts,})
     except:
         return not_found(request, message="Sorry, the tag you are searching for does not exist.")
 
 def posts_by_date(request, year, month):
     posts = Post.objects.filter(date__year=year, date__month=month)[:5]
-    if not posts:
+    if posts:
+        return render_to_response('blog.html', {'posts': posts,})
+    else:
         return not_found(request, message="Sorry, there are no posts written that month.")
-    template = get_template('blog.html')
-    html = template.render(Context({'posts': posts,}))
-    return HttpResponse(html)
