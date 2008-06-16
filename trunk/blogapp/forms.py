e16cf756112ec32de5b62599713967d4f71bc466
jarusauskas <jarusauskas@cb7e2e46-004f-0410-aa2f-81f071f90814>
2008-06-16 22:41:35 +0000

    comment posting added

--- trunk/blogapp/forms.py
+++ trunk/blogapp/forms.py
@@ -1,10 +1,8 @@
-# -*- coding: UTF-8 -*-
-
 from django import newforms as forms
 from django.utils.translation import ugettext as _
 
 class CommentForm(forms.Form):
-    author_name = forms.CharField(label=_('Name'))
+    author_name = forms.CharField(label=_('Name'), max_length=32)
     author_email = forms.EmailField(label=_('E-mail'))
     author_website = forms.URLField(required=False, label=_('Website'))
-    comment = forms.CharField(widget=forms.Textarea, label=_('Comment')
+    comment = forms.CharField(widget=forms.Textarea, label=_('Comment'), max_length=2048)
