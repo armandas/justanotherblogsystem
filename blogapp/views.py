f6ec25208f48e974c0aa40c15e998605c5880a0a
jarusauskas <jarusauskas@cb7e2e46-004f-0410-aa2f-81f071f90814>
2008-06-16 22:41:35 +0000

    comment posting added

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,20 +1,20 @@
-from django.template import RequestContext
 from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.http import HttpResponseRedirect
 from django.utils.translation import ugettext as _
-
+from django.utils.html import escape
 from django.core.paginator import Paginator, InvalidPage
 from django.core.exceptions import ObjectDoesNotExist
+from django.core.urlresolvers import reverse
 
 from blogapp.models import *
 from blogapp.utilities import *
-#from blogapp.forms import CommentForm
+from blogapp.forms import CommentForm
+
+from datetime import datetime
 
 BLOG_TPL = 'blog.html'
 P_LIMIT = int(options('posts_per_page'))
-
-#def comment(request):
-    #form = CommentForm()
-    #return render_to_response('blank.html', {'source': form.as_p()}, context_instance=RequestContext(request))
 
 def homepage(request):
     pg = Paginator(Post.objects.all(), P_LIMIT)
@@ -30,15 +30,33 @@ def homepage(request):
 def post_by_name(request, post_name):
     try:
         post = Post.objects.get(name=post_name)
-        comments = post.comment_set.all()
-        context = {
-            'posts': [post],
-            'comments': comments,
-            'title': post.title,
-            }
-        return render_to_response(BLOG_TPL, context, context_instance=RequestContext(request))
     except ObjectDoesNotExist:
         return not_found(request, message=_("Sorry, the requested post does not exist."))
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            p = Comment(author_name=form.cleaned_data['author_name'],
+                        author_email=form.cleaned_data['author_email'],
+                        author_website=form.cleaned_data.get('author_website', ''),
+                        content=escape(form.cleaned_data['comment']),
+                        date=datetime.now(),
+                        author_ip=request.META['REMOTE_ADDR'],
+                        post=post
+                        )
+            p.save()
+            return HttpResponseRedirect(reverse('blogapp.views.post_by_name', args=[post.name]))
+    else:
+        form = CommentForm()
+
+    comments = post.comment_set.all()
+    context = {
+        'posts': [post],
+        'comments': comments,
+        'title': post.title,
+        'comment_form': form,
+        }
+    return render_to_response(BLOG_TPL, context, context_instance=RequestContext(request))
 
 def posts_by_tag(request, tag_name):
     try:
