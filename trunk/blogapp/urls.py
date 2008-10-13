e86471db5d1847bc5bc62e5a0d559b79728172df
jarusauskas <jarusauskas@cb7e2e46-004f-0410-aa2f-81f071f90814>
2008-10-13 19:15:05 +0000

    bugfix: underscores were not allowed according to urls.py

--- trunk/blogapp/urls.py
+++ trunk/blogapp/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('blogapp.views',
     (r'^$', 'homepage'),
-    (r'^([a-z0-9-]+).html$', 'post_by_name'),
-    (r'^page/([a-z0-9-]+).html$', 'page_by_name'),
-    (r'^tag/([a-z0-9-]+)/$', 'posts_by_tag'),
+    (r'^([a-z0-9_-]+).html$', 'post_by_name'),
+    (r'^page/([a-z0-9_-]+).html$', 'page_by_name'),
+    (r'^tag/([a-z0-9_-]+)/$', 'posts_by_tag'),
     (r'^archive/([0-9]{4})/([0-9]{1,2})/$', 'posts_by_date'),
     (r'feed/(rss|atom)/$', 'feed'),
 )
