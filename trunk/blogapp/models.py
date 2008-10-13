511409ed1bc987ef47eebcd1b0d631f5d36ebd6a
jarusauskas <jarusauskas@cb7e2e46-004f-0410-aa2f-81f071f90814>
2008-10-13 19:28:39 +0000

    feature: slugs (names) are now auto generated from title

--- trunk/blogapp/models.py
+++ trunk/blogapp/models.py
@@ -1,19 +1,34 @@
 from django.db import models
+from unicodedata import normalize
+from re import sub
 
+def slugify(title):
+    name = normalize('NFKD', title).encode('ascii', 'ignore').replace(' ', '-').lower()
+    #remove `other` characters
+    name = sub('[^a-zA-Z0-9_-]', '', name)
+    #nomalize dashes
+    name = sub('-+', '-', name)
+
+    return name
 
 class Tag(models.Model):
     title = models.CharField(max_length=32)
-    name = models.CharField(max_length=32)
+    name = models.CharField(max_length=32, help_text="Leave empty for auto generation.", blank=True)
 
     def __unicode__(self):
         return self.name
+
+    def save(self, force_insert=False, force_update=False):
+        if not self.name:
+            self.name = slugify(self.title)
+        super(Post, self).save(force_insert, force_update)
 
     class Meta:
         ordering = ["name"]
 
 class Post(models.Model):
     title = models.CharField(max_length=256)
-    name = models.CharField(max_length=256)
+    name = models.CharField(max_length=256, help_text="Leave empty for auto generation.", blank=True)
     content = models.TextField(max_length=8192)
     date = models.DateTimeField()
     disable_comments = models.BooleanField()
@@ -22,17 +37,27 @@ class Post(models.Model):
     def __unicode__(self):
         return self.name
 
+    def save(self, force_insert=False, force_update=False):
+        if not self.name:
+            self.name = slugify(self.title)
+        super(Post, self).save(force_insert, force_update)
+
     class Meta:
         ordering = ["-date"]
 
 class Page(models.Model):
     title = models.CharField(max_length=256)
-    name = models.CharField(max_length=256)
+    name = models.CharField(max_length=256, help_text="Leave empty for auto generation.", blank=True)
     content = models.TextField(max_length=8192)
     date = models.DateTimeField()
 
     def __unicode__(self):
         return self.name
+
+    def save(self, force_insert=False, force_update=False):
+        if not self.name:
+            self.name = slugify(self.title)
+        super(Post, self).save(force_insert, force_update)
 
     class Meta:
         ordering = ["name"]
